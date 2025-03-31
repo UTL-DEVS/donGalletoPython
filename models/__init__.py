2b2324af226053169b5c20b9975139dea320fc7d
joel-briones-B-Low <joellbriones701@gmail.com>
2025-03-30 23:46:37 -0600

    correcion inicializaciones

--- models/__init__.py
+++ models/__init__.py
@@ -2,3 +2,6 @@ from .usuario import Usuario
 from .usuario import PreRegistro
 from .persona import Persona
 from .proveedor import Proveedor
+from .materiaPrima import MateriaPrima
+from .galleta import Galleta
+from .receta import Receta, detalleReceta
