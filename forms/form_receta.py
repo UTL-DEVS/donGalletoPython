96168690ed5a1df9400510b197d7dd80f877b4f7
IDGS-801-22001037 <idgs.801.22001037@gmail.com>
2025-03-31 13:36:12 -0600

    crud recetas hecho

--- forms/form_receta.py
+++ forms/form_receta.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import IntegerField, FloatField, SelectField, SubmitField
+from wtforms import IntegerField, FloatField, SelectField, SubmitField, StringField
 from wtforms.validators import DataRequired
 from models import MateriaPrima, Galleta
 
@@ -11,3 +11,18 @@ class DetalleRecetaForm(FlaskForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.id_materia.choices = [(mp.id_materia, mp.nombre_materia) for mp in MateriaPrima.query.all()]
+        
+
+
+class RecetaForm(FlaskForm):
+    nombre_receta = StringField('Nombre de la Receta', validators=[DataRequired()])
+    id_galleta = SelectField('Galleta', coerce=int, validators=[DataRequired()])
+    id_materia = SelectField('Materia Prima', coerce=int, validators=[DataRequired()])
+    cantidad_insumo = FloatField('Cantidad por Materia Prima', validators=[DataRequired()])
+    submit = SubmitField('Agregar Receta')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.id_materia.choices = [(mp.id_materia, mp.nombre_materia) for mp in MateriaPrima.query.all()]
+        self.id_galleta.choices = [(g.id_galleta, g.nombre_galleta) for g in Galleta.query.filter_by(activo=True).all()]
+
