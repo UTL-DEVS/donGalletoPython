1c634ef707b4d521df86db061665d97f243b6234
Carlos <21000405@alumnos.utleon.edu.mx>
2025-03-31 20:42:15 -0600

    correcciones bp

--- routes/__init__.py
+++ routes/__init__.py
@@ -10,14 +10,4 @@ from routes.detalle_produccion import detalle_produccion_bp
 from routes.recetas import recetas_bp
 from routes.resum import resumen_bp
 from routes.gallet import galleta_bp
-from routes.resum import resumen_bp
-from routes.cliente import cliente_bp
-from routes.usuario import usuario_bp
-from routes.proveedor import proveedor_bp
-from routes.cliente import cliente_bp
-from routes.economia import economia_bp
-from routes.recetas import recetas_bp
-from routes.gallet import galleta_bp
-from routes.resum import resumen_bp
-from routes.cliente import cliente_bp
 from routes.usuario import usuario_bp
