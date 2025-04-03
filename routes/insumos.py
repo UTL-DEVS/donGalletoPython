860b4c6c40fcf14e1b0dc48bd60cfb546eda1b65
Arath Andre Tavera Estrada <81714@alumnos.utleon.edu.mx>
2025-04-03 08:32:52 -0600

    proteccion de rutas, registro de errores y actividad

--- routes/insumos.py
+++ routes/insumos.py
@@ -1,8 +1,10 @@
-from flask import Blueprint, render_template, redirect, url_for, flash, request
+from utils import Blueprint, render_template, redirect, flash, db, url_for, request, login_required, current_user, abort
 from models.materiaPrima import MateriaPrima
 from models.proveedor import Proveedor
 from utils.db import db
 from forms.form_insumo import InsumoForm
+from funcs import crear_log_user, crear_log_error
+
 
 insumos_bp = Blueprint('insumos', __name__, url_prefix='/insumos', template_folder='templates')
 
@@ -29,111 +31,151 @@ def convertir_a_base(cantidad, unidad):
         4: 1000,   
     }
     return cantidad * conversiones.get(unidad, 1)
+
 @insumos_bp.route('/insumo', methods=['GET', 'POST'])
+@login_required
 def vista_admin_insumos():
-    # Obtener todos los insumos
-    insumos = MateriaPrima.query.all()
-    form = InsumoForm()
-    form.unidad_medida.choices = list(UNIDADES.items())
-    form.unidad_medida_publico.choices = list(UNIDADES_PUBLICAS.items())
-    
-    # Obtener proveedores disponibles
-    proveedores = Proveedor.query.all()
-    form.id_proveedor.choices = [(p.id_proveedor, p.nombre_proveedor) for p in proveedores]
-    if form.validate_on_submit():
-        nombre = form.nombre.data
-        cantidad = float(form.stock_materia.data)
-        unidad_base = form.unidad_medida.data
-        unidad_publico = form.unidad_medida_publico.data
-        precio = float(form.precio.data)
-        id_proveedor = form.id_proveedor.data
-        cantidad_convertida = convertir_a_base(cantidad, unidad_base)
+    if current_user.rol_user != 0:
+        abort(404)
+    try:
+        crear_log_user(current_user.usuario, request.url)
 
-        # Crear un nuevo insumo
-        nuevo_insumo = MateriaPrima(
-            nombre_materia=nombre,
-            stock_materia=cantidad_convertida,
-            unidad_medida=unidad_base,
-            unidad_medida_publico=unidad_publico,
-            precio=precio,
-            estatus=1,
-            id_proveedor=id_proveedor,
-        )
-        db.session.add(nuevo_insumo)
-        db.session.commit()
-        flash("✅ Insumo registrado correctamente", "success")
-        return redirect(url_for('insumos.vista_admin_insumos'))
+        insumos = MateriaPrima.query.all()
+        form = InsumoForm()
+        form.unidad_medida.choices = list(UNIDADES.items())
+        form.unidad_medida_publico.choices = list(UNIDADES_PUBLICAS.items())
 
-    return render_template('pages/page-insumo/admin.html', 
-                           insumos=insumos, 
-                           form=form, 
-                           unidades=UNIDADES)
+        proveedores = Proveedor.query.all()
+        form.id_proveedor.choices = [(p.id_proveedor, p.nombre_proveedor) for p in proveedores]
+
+        if form.validate_on_submit():
+            nombre = form.nombre.data
+            cantidad = float(form.stock_materia.data)
+            unidad_base = form.unidad_medida.data
+            unidad_publico = form.unidad_medida_publico.data
+            precio = float(form.precio.data)
+            id_proveedor = form.id_proveedor.data
+            cantidad_convertida = convertir_a_base(cantidad, unidad_base)
+
+            nuevo_insumo = MateriaPrima(
+                nombre_materia=nombre,
+                stock_materia=cantidad_convertida,
+                unidad_medida=unidad_base,
+                unidad_medida_publico=unidad_publico,
+                precio=precio,
+                estatus=1,
+                id_proveedor=id_proveedor,
+            )
+            db.session.add(nuevo_insumo)
+            db.session.commit()
+            flash("✅ Insumo registrado correctamente", "success")
+            return redirect(url_for('insumos.vista_admin_insumos'))
+
+        return render_template('pages/page-insumo/admin.html', 
+                            insumos=insumos, 
+                            form=form, 
+                            unidades=UNIDADES)
+
+    except Exception as e:
+        crear_log_error(current_user.usuario, str(e))
+        flash("❌ Error inesperado al acceder a insumos", "danger")
+        return redirect('/error')
+
 
 
 
 @insumos_bp.route('/insumo/editar/<int:id_materia>', methods=['GET', 'POST'])
+@login_required
 def editar_insumo(id_materia):
-    form = InsumoForm()
-    form.unidad_medida.choices = list(UNIDADES.items())
-    form.unidad_medida_publico.choices = list(UNIDADES_PUBLICAS.items())
-    
-    # Obtener proveedores disponibles
-    proveedores = Proveedor.query.all()
-    form.id_proveedor.choices = [(p.id_proveedor, p.nombre_proveedor) for p in proveedores]
-    inicial = 0
-    insumo_editar = MateriaPrima.query.get_or_404(id_materia)
-    print(insumo_editar)
-    if request.method == 'GET':
-        # Cargar los datos del insumo para editar
-        form.id_materia.data = insumo_editar.id_materia
-        form.nombre.data = insumo_editar.nombre_materia
-        form.stock_materia.data = insumo_editar.stock_materia
-        form.unidad_medida.data = insumo_editar.unidad_medida
-        form.unidad_medida_publico.data = insumo_editar.unidad_medida_publico
-        form.precio.data = insumo_editar.precio
-        form.id_proveedor.data = insumo_editar.id_proveedor
-        inicial = form.stock_materia.data
-    if form.validate_on_submit():
-        nombre = form.nombre.data
-        cantidad = float(form.stock_materia.data)
-        unidad_base = form.unidad_medida.data
-        unidad_publico = form.unidad_medida_publico.data
-        precio = float(form.precio.data)
-        id_proveedor = form.id_proveedor.data
-        if insumo_editar.stock_materia != cantidad or insumo_editar.unidad_medida != unidad_base:
-            
-            cantidad_convertida = convertir_a_base(cantidad, unidad_base)
-        else:
-            cantidad_convertida = cantidad
-        
-        if unidad_publico == 4:
-            cantidad_convertida = cantidad_convertida * 1000
-        else:
-            cantidad_convertida = cantidad_convertida
-                
-        # Actualizar insumo existente
-        insumo_editar.nombre_materia = nombre
-        insumo_editar.stock_materia = cantidad_convertida
-        insumo_editar.unidad_medida = unidad_base
-        insumo_editar.unidad_medida_publico = unidad_publico
-        insumo_editar.precio = precio
-        insumo_editar.id_proveedor = id_proveedor
+    if current_user.rol_user != 0:
+        abort(404)
+    try:
+        crear_log_user(current_user.usuario, request.url)
 
-        db.session.commit()
-        flash("✅ Insumo actualizado correctamente", "success")
-        return redirect(url_for('insumos.vista_admin_insumos'))
+        form = InsumoForm()
+        form.unidad_medida.choices = list(UNIDADES.items())
+        form.unidad_medida_publico.choices = list(UNIDADES_PUBLICAS.items())
 
-    return render_template('pages/page-insumo/admin.html', form=form)
+        proveedores = Proveedor.query.all()
+        form.id_proveedor.choices = [(p.id_proveedor, p.nombre_proveedor) for p in proveedores]
+        inicial = 0
+        insumo_editar = MateriaPrima.query.get_or_404(id_materia)
+
+        if request.method == 'GET':
+            form.id_materia.data = insumo_editar.id_materia
+            form.nombre.data = insumo_editar.nombre_materia
+            form.stock_materia.data = insumo_editar.stock_materia
+            form.unidad_medida.data = insumo_editar.unidad_medida
+            form.unidad_medida_publico.data = insumo_editar.unidad_medida_publico
+            form.precio.data = insumo_editar.precio
+            form.id_proveedor.data = insumo_editar.id_proveedor
+            inicial = form.stock_materia.data
+
+        if form.validate_on_submit():
+            nombre = form.nombre.data
+            cantidad = float(form.stock_materia.data)
+            unidad_base = form.unidad_medida.data
+            unidad_publico = form.unidad_medida_publico.data
+            precio = float(form.precio.data)
+            id_proveedor = form.id_proveedor.data
+
+            if insumo_editar.stock_materia != cantidad or insumo_editar.unidad_medida != unidad_base:
+                cantidad_convertida = convertir_a_base(cantidad, unidad_base)
+            else:
+                cantidad_convertida = cantidad
+
+            if unidad_publico == 4:
+                cantidad_convertida = cantidad_convertida * 1000
+
+            insumo_editar.nombre_materia = nombre
+            insumo_editar.stock_materia = cantidad_convertida
+            insumo_editar.unidad_medida = unidad_base
+            insumo_editar.unidad_medida_publico = unidad_publico
+            insumo_editar.precio = precio
+            insumo_editar.id_proveedor = id_proveedor
+
+            db.session.commit()
+            flash("✅ Insumo actualizado correctamente", "success")
+            return redirect(url_for('insumos.vista_admin_insumos'))
+
+        return render_template('pages/page-insumo/admin.html', form=form)
+
+    except Exception as e:
+        crear_log_error(current_user.usuario, str(e))
+        flash("❌ Error al editar insumo", "danger")
+        return redirect('/error')
 
 @insumos_bp.route('/estatus/<int:id>')
+@login_required
 def cambiar_estatus_insumo(id):
-    insumo = MateriaPrima.query.get_or_404(id)
-    insumo.estatus = 0 if insumo.estatus == 1 else 1
-    db.session.commit()
-    flash("Estatus del insumo actualizado ✅", "success")
-    return redirect(url_for('insumos.vista_admin_insumos'))
+    if current_user.rol_user != 0:
+        abort(404)
+    try:
+        crear_log_user(current_user.usuario, request.url)
+
+        insumo = MateriaPrima.query.get_or_404(id)
+        insumo.estatus = 0 if insumo.estatus == 1 else 1
+        db.session.commit()
+        flash("Estatus del insumo actualizado ✅", "success")
+        return redirect(url_for('insumos.vista_admin_insumos'))
+
+    except Exception as e:
+        crear_log_error(current_user.usuario, str(e))
+        flash("❌ Error al cambiar estatus del insumo", "danger")
+        return redirect('/error')
 
 @insumos_bp.route('/cocina')
+@login_required
 def vista_cocina_insumos():
-    insumos = MateriaPrima.query.filter_by(estatus=1).all()
-    return render_template('pages/page-insumo/cocina.html', insumos=insumos, unidades=UNIDADES)
+    if current_user.rol_user != 2:
+        abort(404)
+    try:
+        crear_log_user(current_user.usuario, request.url)
+
+        insumos = MateriaPrima.query.filter_by(estatus=1).all()
+        return render_template('pages/page-insumo/cocina.html', insumos=insumos, unidades=UNIDADES)
+
+    except Exception as e:
+        crear_log_error(current_user.usuario, str(e))
+        flash("❌ Error al cargar insumos en cocina", "danger")
+        return redirect('/error')
--- routes/produccion.py
+++ routes/produccion.py
@@ -1,31 +1,50 @@
-from flask import Blueprint, request, jsonify
+from flask import Blueprint, request, jsonify, abort, login_required, current_user
 import json
 from models import produccion, detalle_produccion
 from controller import  controller_produccion, controller_detalle_produccion
 from datetime import date, datetime
+from funcs import crear_log_user, crear_log_error
+
 
 produccion_bp = Blueprint('produccion', __name__, template_folder='templates')
 
 @produccion_bp.route('/agregarProduccion', methods=['POST'])
+@login_required
 def agregarProduccion():
-    data = request.get_json()
-    objProduccion = produccion.Produccion()
-    objProduccion.fecha_produccion = date.today().strftime('%Y-%m-%d')
-    objProduccion.hora_produccion = datetime.now().strftime('%H:%M:%S')
-    objProduccion.estatus = 1
-    objProduccion.id_produccion = controller_produccion.agregarProduccion(objProduccion)
-    lstDetalleProduccion = data.get('lstDetalleProduccion')
-    for detalleProduccion in lstDetalleProduccion:
-        objDetalleProduccion = detalle_produccion.DetalleProduccion()
-        objDetalleProduccion.id_produccion = objProduccion.id_produccion
-        objDetalleProduccion.id_galleta = detalleProduccion["id_galleta"]
-        objDetalleProduccion.cantidad = detalleProduccion["cantidad"]
-        if objDetalleProduccion.cantidad == 0:
-            continue
-        resultadoDetalleProduccion = controller_detalle_produccion.agregarDetalleProduccion(objDetalleProduccion)
+    if current_user.rol_user != 2:
+        abort(404)
 
-    return jsonify({
-                "success": True,
-                "message": "Se envio correctamente a produccion!"
-            })
+    try:
+        crear_log_user(current_user.usuario, request.url)
+
+        data = request.get_json()
+        objProduccion = produccion.Produccion()
+        objProduccion.fecha_produccion = date.today().strftime('%Y-%m-%d')
+        objProduccion.hora_produccion = datetime.now().strftime('%H:%M:%S')
+        objProduccion.estatus = 1
+        objProduccion.id_produccion = controller_produccion.agregarProduccion(objProduccion)
+
+        lstDetalleProduccion = data.get('lstDetalleProduccion')
+
+        for detalleProduccion in lstDetalleProduccion:
+            objDetalleProduccion = detalle_produccion.DetalleProduccion()
+            objDetalleProduccion.id_produccion = objProduccion.id_produccion
+            objDetalleProduccion.id_galleta = detalleProduccion["id_galleta"]
+            objDetalleProduccion.cantidad = detalleProduccion["cantidad"]
+            if objDetalleProduccion.cantidad == 0:
+                continue
+            controller_detalle_produccion.agregarDetalleProduccion(objDetalleProduccion)
+
+        return jsonify({
+            "success": True,
+            "message": "Se envió correctamente a producción!"
+        })
+
+    except Exception as e:
+        crear_log_error(current_user.usuario, str(e))
+        return jsonify({
+            "success": False,
+            "message": "❌ Error al procesar producción.",
+            "error": str(e)
+        }), 500
 
