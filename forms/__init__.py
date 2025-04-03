f58d59df33258e0e82bf062c3ac2c1cdd33397b0
IDGS-801-22001049 <pinamorenomiltonalfredo84@gmail.com>
2025-04-03 09:20:03 -0600

    ventas_final

--- forms/__init__.py
+++ forms/__init__.py
@@ -4,7 +4,6 @@ from forms.form_persona import form_persona
 from forms.form_login import regis_form
 from forms.form_login import conf_form
 from forms.form_receta import DetalleRecetaForm
-from forms.form_resumen import *
 from forms.form_receta import DetalleRecetaForm, RecetaForm
 from forms.form_galleta import *
 from forms.form_proceso_venta import CarritoForm
