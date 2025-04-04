b16542895e926e14b53198bf7a0818c7252db03b
CharlyPadilla <charlypadia90888@gmail.com>
2025-04-03 18:07:09 -0600

    Corrección de cambios en usuario y proveedores

--- routes/proveedor.py
+++ routes/proveedor.py
@@ -14,78 +14,108 @@ modales = json.loads(modales_str)
 @proveedor_bp.route('/', methods=['POST','GET'])
 @login_required
 def mostrar_proveedores():
-    form_proveedor_obj = form_proveedor(request.form)
-    form_persona_obj = form_persona(request.form)
-    lista_proveedores=controller_proveedor.obtener_proveedores()
-    if current_user.rol_user != 0 :
-        return redirect('/login')
-    return render_template('pages/proveedor.html',modales=json.dumps(modales),form_persona=form_persona_obj,form_proveedor=form_proveedor_obj,lista_proveedores=lista_proveedores)
+    try:
+        if current_user.rol_user != 0:
+            abort(404)
+        form_proveedor_obj = form_proveedor(request.form)
+        form_persona_obj = form_persona(request.form)
+        lista_proveedores=controller_proveedor.obtener_proveedores()
 
+        return render_template('pages/proveedor.html',modales=json.dumps(modales),form_persona=form_persona_obj,form_proveedor=form_proveedor_obj,lista_proveedores=lista_proveedores)
+    except Exception as e:
+        crear_log_error(current_user.usuario, str(e))
+        flash("Error al cargar el panel de proveedores", "danger")
+        return redirect('/error')
+    
 @proveedor_bp.route('/detallesProveedor', methods=['GET'])
 @login_required
 @login_required
 def detalles_proveedor():
-    if current_user.rol_user != 0 :
-        return redirect('/login')
-    form_proveedor_obj = form_proveedor(request.form)
-    form_persona_obj = form_persona(request.form)
-    lista_proveedores=controller_proveedor.obtener_proveedores()
-    proveedor_seleccionado=controller_proveedor.obtener_proveedor_especifico(int(request.args.get('id_prov')))
-    if(str(request.args.get('modal'))=='edit'):
-        modales["editar"]=1
-    else:
-        modales["detalles"]=1
-   
-    return render_template('pages/proveedor.html',modales=json.dumps(modales),form_proveedor=form_proveedor_obj ,form_persona=form_persona_obj,lista_proveedores=lista_proveedores,prov_sel=proveedor_seleccionado)
-    # return redirect(url_for('proveedor.mostrar_proveedores'))
-    #return json.dumps(proveedor_seleccionado.to_dict())
-
+    try:
+        if current_user.rol_user != 0:
+            abort(404)
+        form_proveedor_obj = form_proveedor(request.form)
+        form_persona_obj = form_persona(request.form)
+        lista_proveedores=controller_proveedor.obtener_proveedores()
+        proveedor_seleccionado=controller_proveedor.obtener_proveedor_especifico(int(request.args.get('id_prov')))
+        if(str(request.args.get('modal'))=='edit'):
+            modales["editar"]=1
+        else:
+            modales["detalles"]=1
+    
+        return render_template('pages/proveedor.html',modales=json.dumps(modales),form_proveedor=form_proveedor_obj ,form_persona=form_persona_obj,lista_proveedores=lista_proveedores,prov_sel=proveedor_seleccionado)
+    except Exception as e:
+        crear_log_error(current_user.usuario, str(e))
+        flash("Error al cargar el panel de proveedores", "danger")
+        return redirect('/error')
+    
 @proveedor_bp.route('/actualizarProveedor', methods=['POST','GET'])
 @login_required
 def actualizar_proveedor():
-    if current_user.rol_user != 0 :
-        return redirect('/login')
-    form_persona_obj = form_persona()
-    controller_proveedor.actualizar_proveedor(int(request.args.get('id_prov_upd')),form_persona_obj)
-    
-    return redirect(url_for('proveedor.mostrar_proveedores'))
+    try:
+        if current_user.rol_user != 0:
+            abort(404)
+        if current_user.rol_user != 0 :
+            return redirect('/login')
+        form_persona_obj = form_persona()
+        controller_proveedor.actualizar_proveedor(int(request.args.get('id_prov_upd')),form_persona_obj)
+        crear_log_user(current_user.usuario, request.url)
+        return redirect(url_for('proveedor.mostrar_proveedores'))
+    except Exception as e:
+        crear_log_error(current_user.usuario, str(e))
+        flash("Error al cargar el panel de proveedores", "danger")
+        return redirect('/error')
 
 @proveedor_bp.route('/eliminarProveedor', methods=['POST','GET'])
 @login_required
 def eliminar_proveedor():
-    if current_user.rol_user != 0 :
-        return redirect('/login')
-    controller_proveedor.eliminar_proveedor(int(request.args.get('id_prov_del')))
-    
-    return redirect(url_for('proveedor.mostrar_proveedores'))
+    try:
+        if current_user.rol_user != 0:
+            abort(404)
+        controller_proveedor.eliminar_proveedor(int(request.args.get('id_prov_del')))
+        crear_log_user(current_user.usuario, request.url)
+        return redirect(url_for('proveedor.mostrar_proveedores'))
+    except Exception as e:
+        crear_log_error(current_user.usuario, str(e))
+        flash("Error al cargar el panel económico", "danger")
+        return redirect('/error')
 
 @proveedor_bp.route('/reactivarProveedor', methods=['POST','GET'])
 @login_required
 def reactivarProveedor():
-    if current_user.rol_user != 0 :
-        return redirect('/login')
-    controller_proveedor.reactivar_proveedor(int(request.args.get('id_prov_rea')))
-    return redirect(url_for('proveedor.mostrar_proveedores'))
-
+    try:
+        if current_user.rol_user != 0:
+            abort(404)
+       
+        controller_proveedor.reactivar_proveedor(int(request.args.get('id_prov_rea')))
+        crear_log_user(current_user.usuario, request.url)
+        return redirect(url_for('proveedor.mostrar_proveedores'))
+    except Exception as e:
+        crear_log_error(current_user.usuario, str(e))
+        flash("Error al cargar el panel de proveedores", "danger")
+        return redirect('/error')
 
 
 @proveedor_bp.route('/agregarProveedor', methods=['POST'])
 @login_required
-@login_required
 def agregar_proveedor():
-    if current_user.rol_user != 0 :
-        return redirect('/login')
-    form_proveedor_obj = form_proveedor(request.form)
-    form_persona_obj = form_persona()
-    if request.method=='POST' and form_proveedor_obj.validate_on_submit():
-        controller_proveedor.agregar_proveedor(form_proveedor_obj,form_persona_obj)
-        return redirect(url_for('proveedor.mostrar_proveedores'))
-    else:
-        modales["agregar"]=1
-        lista_proveedores=controller_proveedor.obtener_proveedores()
- 
-    return render_template('pages/proveedor.html',modales=json.dumps(modales),form_persona=form_persona_obj,form_proveedor=form_proveedor_obj,lista_proveedores=lista_proveedores)
-
+    try:
+        if current_user.rol_user != 0:
+            abort(404)
+        form_proveedor_obj = form_proveedor(request.form)
+        form_persona_obj = form_persona()
+        if request.method=='POST' and form_proveedor_obj.validate_on_submit():
+            controller_proveedor.agregar_proveedor(form_proveedor_obj,form_persona_obj)
+            return redirect(url_for('proveedor.mostrar_proveedores'))
+        else:
+            modales["agregar"]=1
+            lista_proveedores=controller_proveedor.obtener_proveedores()
+        crear_log_user(current_user.usuario, request.url)
+        return render_template('pages/proveedor.html',modales=json.dumps(modales),form_persona=form_persona_obj,form_proveedor=form_proveedor_obj,lista_proveedores=lista_proveedores)
+    except Exception as e:
+        crear_log_error(current_user.usuario, str(e))
+        flash("Error al cargar el panel de proveedores", "danger")
+        return redirect('/error')
 
 @proveedor_bp.after_request
 def after_request(response):
--- routes/usuario.py
+++ routes/usuario.py
@@ -1,7 +1,8 @@
-from utils import Blueprint, render_template, redirect, request, login_required, url_for, current_user,flash
+from utils import Blueprint, render_template, redirect, request, login_required, url_for, current_user,flash,abort
 import json
 from forms import *
 from controller import controller_usuario
+from funcs import crear_log_user, crear_log_error
 usuario_bp = Blueprint('usuario', __name__,  url_prefix='/navegante' ,template_folder='templates')
 
 modales_str = '{"detalles": 0, "editar": 0, "agregar": 0}'  
@@ -10,82 +11,98 @@ modales = json.loads(modales_str)  # Convertir a diccionario
 @usuario_bp.route('/empleado', methods=['POST','GET'])
 @login_required
 def mostrar_empleados():
-    if current_user.rol_user != 0 :
-        abort(404)
+    try:
+        if current_user.rol_user != 0:
+            abort(404)
      
-    form_empleado_obj = form_empleado()
-    lista_empleados=controller_usuario.obtener_empleados()
+        form_empleado_obj = form_empleado()
+        lista_empleados=controller_usuario.obtener_empleados()
+        
+        return render_template('pages/page-usuario/emlpeado.html',modales=json.dumps(modales),form_empleado=form_empleado_obj,lista_empleados=lista_empleados)
+    except Exception as e:
+        crear_log_error(current_user.usuario, str(e))
+        flash("Error al cargar el panel de usuarios", "danger")
+        return redirect('/error')
     
-    return render_template('pages/page-usuario/emlpeado.html',modales=json.dumps(modales),form_empleado=form_empleado_obj,lista_empleados=lista_empleados)
-
 @usuario_bp.route('/agregarEmpleado', methods=['POST'])
 @login_required
 def agregar_proveedor():
-    if current_user.rol_user != 0 :
-        abort(404)
-    form_empleado_obj = form_empleado(request.form)
-
-    if request.method=='POST'and form_empleado_obj.validate_on_submit():
-        if (controller_usuario.agregar_empleado(form_empleado_obj)):
-            flash("Empleado agregado con éxito", "success")
-        return redirect(url_for('usuario.mostrar_empleados'))
-    modales["agregar"]=1
-    lista_empleados=controller_usuario.obtener_empleados()
-
-    print(form_empleado_obj.data)  # Ver qué datos está recibiendo el formulario
-    print(form_empleado_obj.errors) 
-    return render_template('pages/page-usuario/emlpeado.html',modales=json.dumps(modales), form_empleado=form_empleado_obj, lista_empleados=lista_empleados)
-
+    try:
+        if current_user.rol_user != 0:
+            abort(404)
+        form_empleado_obj = form_empleado(request.form)
+        if request.method=='POST'and form_empleado_obj.validate_on_submit():
+            if (controller_usuario.agregar_empleado(form_empleado_obj)):
+                flash("Empleado agregado con éxito", "success")
+            return redirect(url_for('usuario.mostrar_empleados'))
+        modales["agregar"]=1
+        lista_empleados=controller_usuario.obtener_empleados()
+        crear_log_user(current_user.usuario, request.url)
+        return render_template('pages/page-usuario/emlpeado.html',modales=json.dumps(modales), form_empleado=form_empleado_obj, lista_empleados=lista_empleados)
+    except Exception as e:
+        crear_log_error(current_user.usuario, str(e))
+        flash("Error al cargar el panel de usuarios", "danger")
+        return redirect('/error')
 
 @usuario_bp.route('/empleado/detallesEmpleado', methods=['GET'])
 @login_required
 def detalles_empleado():
-    if current_user.rol_user != 0 :
-        abort(404)
-    form_empleado_obj = form_empleado(request.form)
-    lista_empleados=controller_usuario.obtener_empleados()
-    empleado_seleccionado=controller_usuario.obtener_empleado_especifico(int(request.args.get('id_emp')))
-    if(str(request.args.get('modal'))=='edit'):
-        modales["editar"]=1
-    else:
-        modales["detalles"]=1
-    print(f'sel: {empleado_seleccionado.persona.nombre}')
-    return render_template('pages/page-usuario/emlpeado.html',modales=json.dumps(modales) ,form_empleado=form_empleado_obj,lista_empleados=lista_empleados,emp_sel=empleado_seleccionado)
-
+    try:
+        if current_user.rol_user != 0:
+            abort(404)
+        form_empleado_obj = form_empleado(request.form)
+        lista_empleados=controller_usuario.obtener_empleados()
+        empleado_seleccionado=controller_usuario.obtener_empleado_especifico(int(request.args.get('id_emp')))
+        if(str(request.args.get('modal'))=='edit'):
+            modales["editar"]=1
+        else:
+            modales["detalles"]=1
+        return render_template('pages/page-usuario/emlpeado.html',modales=json.dumps(modales) ,form_empleado=form_empleado_obj,lista_empleados=lista_empleados,emp_sel=empleado_seleccionado)
+    except Exception as e:
+        crear_log_error(current_user.usuario, str(e))
+        flash("Error al cargar el panel de usuarios", "danger")
+        return redirect('/error')
 
 @usuario_bp.route('/actualizarEmpleado', methods=['POST','GET'])
 @login_required
 def actualizar_empleado():
-    if current_user.rol_user == 0 :
-        abort(404)
-    form_empleado_obj = form_empleado(request.form)
-    controller_usuario.actualizar_empleado(int(request.args.get('id_emp_upd')),form_empleado_obj)
-    return redirect(url_for('usuario.mostrar_empleados'))
-
+    try:
+        if current_user.rol_user != 0:
+            abort(404)
+        form_empleado_obj = form_empleado(request.form)
+        controller_usuario.actualizar_empleado(int(request.args.get('id_emp_upd')),form_empleado_obj)
+        crear_log_user(current_user.usuario, request.url)
+        return redirect(url_for('usuario.mostrar_empleados'))
+    except Exception as e:
+        crear_log_error(current_user.usuario, str(e))
+        flash("Error al cargar el panel de usuarios", "danger")
+        return redirect('/error')
 
 @usuario_bp.route('/eliminarProveedor', methods=['POST','GET'])
 @login_required
 def eliminar_proveedor():
-    if current_user.rol_user != 0:
-        abort(404)
     try:
+        if current_user.rol_user != 0:
+            abort(404)
         crear_log_user(current_user.usuario, request.url)
         controller_usuario.eliminar_proveedor(int(request.args.get('id_prov_del')))
         return redirect(url_for('proveedor.mostrar_proveedores'))
     except Exception as e:
         crear_log_error(current_user.usuario, str(e))
+        flash("Error al cargar el panel de usuarios", "danger")
         return redirect('/error')
 
 @usuario_bp.route('/reactivarProveedor', methods=['POST','GET'])
 def reactivarProveedor():
-    if current_user.rol_user != 0:
-        abort(404)
     try:
+        if current_user.rol_user != 0:
+            abort(404)
         crear_log_user(current_user.usuario, request.url)
         controller_usuario.reactivar_proveedor(int(request.args.get('id_prov_rea')))
         return redirect(url_for('proveedor.mostrar_proveedores'))
     except Exception as e:
         crear_log_error(current_user.usuario, str(e))
+        flash("Error al cargar el panel de usuarios", "danger")
         return redirect('/error')
 
 
